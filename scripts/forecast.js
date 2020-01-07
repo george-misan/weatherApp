3d0a7decbf71ec99efcf21b690648f0f796eaddc
iamgeorge <george.eyoyibo@gmail.com>
2020-01-07 07:01:04 +0200

    serving fetch content over HTTPS for Netlify deploy

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,8 +1,8 @@
 class Forecast {
     constructor (){
         this.key = 'CUZ3uTAeRY575Anhb7UsZgTwDHmJImuK';
-        this.weatherURI = 'http://dataservice.accuweather.com/currentconditions/v1/';
-        this.cityURI = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+        this.weatherURI = 'https://dataservice.accuweather.com/currentconditions/v1/';
+        this.cityURI = 'https://dataservice.accuweather.com/locations/v1/cities/search';
     }
     async updateCity (city){
         const cityDets = await this.getCity(city);
